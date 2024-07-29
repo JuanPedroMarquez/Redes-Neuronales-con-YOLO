37eb27977d17495cfd464c678744b506cedc6bea
Juan Pedro Márquez Gandía <juanverdedenoviembre@gmail.com>
2024-07-29 13:00:56 +0200

    Update
    Correcciones varias

--- yolo.py
+++ yolo.py
@@ -2,12 +2,12 @@ from ultralytics import YOLO
 import cv2
 import math
 
-# iniciar la camara y establecer la resoluci'on de la imagen
+# iniciamos la cámara y establecemos la resolución de la imagen
 cap = cv2.VideoCapture(0)
 cap.set(3, 720)
 cap.set(4, 720)
 
-#cargar nuestro modelo
+#cargamos el modelo
 model = YOLO('yolo-Weights/yolov8n.pt')
 
 #definimos las clases que queremos detectar
@@ -30,15 +30,15 @@ while True:
 
     #iteramos sobre lo que detecte yolo
     for r in results:
-        boxes = r.boxes #obtener las cajas de los objetos detectados
+        boxes = r.boxes #obtenemos las cajas de los objetos detectados
         for box in boxes:
-            x1,y1,x2,y2 = box.xyxy[0] #obtener las coordenadas de la caja
-            x1,y1,x2,y2 = map(int, [x1,y1,x2,y2]) #convertir las coordenadas a enteros
+            x1,y1,x2,y2 = box.xyxy[0] #obtenemos las coordenadas de la caja
+            x1,y1,x2,y2 = map(int, [x1,y1,x2,y2]) #convertimos las coordenadas a enteros
 
-            #dibujar la caja en la imagen
+            #dibujamos la caja en la imagen
             cv2.rectangle(img, (x1,y1), (x2,y2), (255,0,255), 3)
 
-            #calcular la confianza del objeto detectado
+            #calculamos la confianza del objeto detectado
             confidence = math.ceil(box.conf[0] * 100) 
             print(confidence)
 
@@ -46,7 +46,7 @@ while True:
             cls = int(box.cls[0])
             print(classNames[cls])
 
-            #escribir el nombre del objeto en la imagen
+            #escribimos el nombre del objeto en la imagen
             org = (x1, y1)
             font = cv2.FONT_HERSHEY_SIMPLEX
             cv2.putText(img, f"{classNames[cls]} {confidence:.2f}", org, font, 1, (255, 0, 0), 2)
